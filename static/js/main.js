641d823491efc4052d3b48ad46e461e632c3e185
Balint Bakcsa <balintbakcsa@gmail.com>
2018-02-07 14:36:08 +0100

    renamed showBoards to showBoard (name was misleading)

--- static/js/main.js
+++ static/js/main.js
@@ -1,18 +1,20 @@
 // This function is to initialize the application
 function init() {
     DataHandler.init();
+
     let boards = DataHandler._data.boards;
     if (boards !== undefined && boards.length > 0) {
         for (let i = 0; i < boards.length; i++)
         {
-            DOM.showBoards(boards[i]);
+            DOM.showBoard(boards[i]);
         }
     }
+
     let newBoardButton = document.getElementById(DOM.Constants.CreateBoardIDs.BUTTON_ID);
     newBoardButton.addEventListener('click', function() {
         let title = document.getElementById(DOM.Constants.CreateBoardIDs.INPUT_ID).value;
         document.getElementById(DOM.Constants.CreateBoardIDs.INPUT_ID).value = "";
-        DataHandler.createNewBoard(title, DOM.showBoards);
+        DataHandler.createNewBoard(title, DOM.showBoard);
     });
     // it uses the dom.js to show boards
 }
